1c04e16e66a3184d0a4b68db5c37139406e579d9
D136073 <diegotetesta@gmail.com>
2023-09-21 12:54:39 +0200

    subiendo agregado de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowseRouter, Routes, Route } from 'react-router-dom';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Header from './componets/layout/Header';
 import Nav from './componets/layout/Nav';
 import Footer from './componets/layout/Footer';
 
-import CotactoPage from './pages/ContactoPage';
+import ContactoPage from './pages/ContactoPage';
 import HomePage from './pages/HomePage';
 import NosotrosPage from './pages/Nosotros Page';
-import NovedadesPage from '/pages/NovedadesPage';
+import NovedadesPage from './pages/NovedadesPage';
 
 import './App.css';
-import ContactoPage from './pages/ContactoPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <BrowseRouter>
+      <BrowserRouter>
         <Nav />  
         <Routes>
           <Route path='/' element={<HomePage />} />
@@ -24,7 +25,7 @@ function App() {
           <Route path='novedades' element={<NovedadesPage />} />
           <Route path='contacto' element={<ContactoPage />} />
         </Routes>
-      </BrowseRouter>
+      </BrowserRouter>
       <Footer />
     </div>
   );
