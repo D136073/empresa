7be66f0392045cee093bf46ca2e726addd22b19e
Diego Testa <80824875+D136073@users.noreply.github.com>
2023-11-23 18:49:39 +0100

    frontend completo para entrega final
    frontend con 4 seccones, envio de mail desde el formulario de contacto, una seccion de noticias que se actualiza desde el backend con imagenes y la seccion hoe y nosotros.
    NO ES EL MEJOR DISEÑO pero cumple con lo pedido para la entrega del curso de full stack con react,js

--- frontend/src/pages/ContactoPage.js
+++ frontend/src/pages/ContactoPage.js
@@ -1,52 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './../styles/components/pages/ContactoPage.css';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
+
+ 
 
 
 
 const ContactoPage = (props) => {
-    return (
-        <>
-        
-         <Form>
-            <h2 id='titulocontacto'>FORMULARIO DE CONTACTO</h2>
-                <Form.Group as={Col} controlId="Line 1" id='row' >
-                    <Form.Label>Email</Form.Label>
-                    <span> </span>
-                    <Form.Control type="email" placeholder="Ingrese email" />
-                    <span> - </span>
-                    <Form.Label>Telefono</Form.Label>
-                    <span> </span>
-                    <Form.Control type="text" placeholder="telefono de contacto"/>
-                </Form.Group>
+    
+    const initialForm = {
+        nombre: '',
+        apellido: '',
+        email: '',
+        mensaje: '',
+        telefono: '',
+    }
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
 
-                <Form.Group className="mb-3" controlId="Line 2" id='row' >
-                    <Form.Label>Nombre</Form.Label>
-                        <span> </span>
-                    <Form.Control placeholder="Nombre" />
-                        <span> - </span>
-                    <Form.Label>Apellido</Form.Label>
-                        <span> </span>
-                    <Form.Control placeholder="Apellido" />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formGridCity">
-                    <Form.Label column sm={2}>Consulta:</Form.Label>
-                </Form.Group>
-                <br></br>
-                <Col className="mb-3">
-                    <textarea className='text-wrap' placeholder='Escriba aqui su consulta' ></textarea>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Acepto enviar mis dato de contacto" />
-                </Form.Group>
-                </Col>
-                <br></br> 
-            <Button variant="primary" type="submit">
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setFormData(oldData =>({
+            ...oldData, 
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e =>{
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        const response = await 
+        axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if(response.data.error === false){
+            setFormData(initialForm);
+        }
+    }
+    return (
+            <form className='formulario' onSubmit={handleSubmit}>
+            <h2 id='titulocontacto'>FORMULARIO DE CONTACTO</h2>
+                <p>
+                    <label>Nombre</label>
+                    <span> </span>
+                    <input type='text' name='nombre' value={formData.nombre} onChange={handleChange} required />
+                    <span> </span>
+                    <label>Apellido</label>
+                    <span> </span>       
+                    <input  name='apellido' type="text" value={formData.apellido} onChange={handleChange} required />
+                </p>
+                <p>
+                    <label>Email</label>
+                    <span> </span>       
+                    <input  name='email' type="email" value={formData.email} onChange={handleChange} required />
+                    <span> </span>      
+                    <label>Telefono</label>
+                    <span> </span>       
+                    <input  name='telefono' type="text" value={formData.telefono} onChange={handleChange} required />
+                </p>
+                <p>   
+                    <label>Consulta</label>  
+                </p>
+                <p>
+                    <textarea  name='mensaje' required className='text-wrap' placeholder='Escriba aqui su consulta' value={formData.mensaje} onChange={handleChange} / > 
+                </p>
+                {sending ? <p>Enviando...</p>: null}
+                {msg ? <p>{msg}</p> : null}
+            <Button variant="primary" type="submit" id='Button'>
                 Enviar
             </Button>
-        </Form>
-        </>
+
+            </form>
         
     );
 }
--- frontend/src/pages/Nosotros Page.js
+++ frontend/src/pages/Nosotros Page.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import '../../src/styles/components/pages/NosotrosPage.css';
+import Card from 'react-bootstrap/Card';
+import Stack from 'react-bootstrap/Stack';
 
 
 const NosotrosPage = (props) => {
     return (
-        <div id='nos'>
-            <h1>Esta sera mi Pagina Nosotros</h1>
-        </div>
+    <div id='contenedornos'>
+        <br/>
+        <Card>
+            <Card.Body>
+                <Card.Img src='images/escritorioplano.jpeg' id='cardimg' alt='unaimagen' height={200}/>
+                <Card.Img src='images/grip.png' id='cardimg' alt='unaimagen' height={200}/>
+                <Card.Img src='images/appmatra2.jpg' id='cardimg' alt='unaimagen' height={200}/>
+                <Card.Img src='images/desarrollo.jpeg' id='cardimg' alt='unaimagen' height={200}/>
+                <Card.Text>
+                    <p>Trabajamos con tecnologias de vanguardia para el desarrollo
+                        deaplicaciones, diseño grafico, diseño de marca y soluciones informaticas.
+                        <br/>
+                        Somos un equipo de profesionales que trabajamos unidos pero en diferentes partes del mundo 
 
+                    </p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+      
+            
+    </div>    
     );
 }
 export default NosotrosPage;
