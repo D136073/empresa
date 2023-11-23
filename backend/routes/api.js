f124c5f9f7f98d94a0e51ff324ce1b6437cb77b5
Diego Testa <80824875+D136073@users.noreply.github.com>
2023-11-23 18:47:27 +0100

    backend completo para entrega final
    backend super minimalista completo para la entrega final del curso de diplomatura fullstack con react.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var novedadesModel = require('./../models/novedadesModels');
 const util = require('util');
 const cloudinary = require('cloudinary').v2;
+const nodemailer = require('nodemailer');
+
 
 
 /* GET home page. */
@@ -29,4 +31,30 @@ router.get('/novedades', async function(req, res, next) {
 
   res.json(novedades);
 });
+
+router.post('/contacto', async (req, res) =>{
+  const mail = {
+    from: req.body.email,
+    to: 'diegotetesta@gmail.com',
+    subject: 'Contacto desde la web',
+    html: `${req.body.nombre} ${req.body.apellido} se contacto a través del formulario web y dejó el siguiente mensaje: ${req.body.mensaje} <br>
+    Su email es: ${req.body.email} y su teléfono es: ${req.body.telefono}`
+  }
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+    error:false,
+    message: 'Mensaje Enviado'
+  });
+});
+
+
 module.exports = router;
